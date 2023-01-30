e5e45f309948a3e6c65bde572b0a35c3764e07c2
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2023-01-30 16:48:05 +0000

    Made the code a bit more dry

--- CircleLayer.java
+++ CircleLayer.java
@@ -1,37 +1,15 @@
 import java.awt.image.*;
 import java.awt.*;
 
-class CircleLayer extends LayerData {
-	private CircleGraphics circleGraphics;
-	private Color strokeCol;
-
-	public CircleLayer(BufferedImage layer) {super(layer);}
-	public CircleLayer(BufferedImage layer, Point layerPos) {super(layer, layerPos);}
+class CircleLayer extends ShapeLayer {
 	public CircleLayer(int width, int height, Color col) {super(width, height, col);}
 	public CircleLayer(int width, int height, Color col, Point layerPos) {super(width, height, col, layerPos);}
 
-	public void resize(int width, int height) {
-		circleGraphics = new CircleGraphics(new Point(0, 0));
-		if (width == 0 || height == 0) return;
-
-		setImage(new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB));
-		Graphics2D g2d = getLayerGraphics();
-
+	public SpecificGraphic getSpecificGraphic(int width, int height) {
+		CircleGraphics circleGraphics = new CircleGraphics(new Point(0, 0));
 		circleGraphics.setDimension(width, height);
 		circleGraphics.setColor(strokeCol);
 	
-		updateGraphics(circleGraphics);
-		updateSelectionLayer();
-	}
-
-	public void resize(Point newLayerEndPos) {
-		int layerWidth = Math.abs(newLayerEndPos.x - getX());
-		int layerHeight = Math.abs(newLayerEndPos.y - getY());
-		resize(layerWidth, layerHeight);
-		setLocation(validPoint(getCoords(), newLayerEndPos));
-	}
-
-	public void setStrokeCol(Color col) {
-		this.strokeCol = col;
+		return circleGraphics;
 	}
 }
--- RectangleLayer.java
+++ RectangleLayer.java
@@ -1,37 +1,15 @@
 import java.awt.image.*;
 import java.awt.*;
 
-class RectangleLayer extends LayerData {
-	private RectangleGraphics rectangleGraphics;
-	private Color strokeCol;
-
-	public RectangleLayer(BufferedImage layer) {super(layer);}
-	public RectangleLayer(BufferedImage layer, Point layerPos) {super(layer, layerPos);}
+class RectangleLayer extends ShapeLayer {
 	public RectangleLayer(int width, int height, Color col) {super(width, height, col);}
 	public RectangleLayer(int width, int height, Color col, Point layerPos) {super(width, height, col, layerPos);}
 
-	public void resize(int width, int height) {
-		rectangleGraphics = new RectangleGraphics(new Point(0, 0));
-		if (width == 0 || height == 0) return;
-
-		setImage(new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB));
-		Graphics2D g2d = getLayerGraphics();
-
+	public SpecificGraphic getSpecificGraphic(int width, int height) {
+		RectangleGraphics rectangleGraphics = new RectangleGraphics(new Point(0, 0));
 		rectangleGraphics.setDimensions(width, height);
 		rectangleGraphics.setColor(strokeCol);
 	
-		updateGraphics(rectangleGraphics);
-		updateSelectionLayer();
-	}
-
-	public void resize(Point newLayerEndPos) {
-		int layerWidth = Math.abs(newLayerEndPos.x - getX());
-		int layerHeight = Math.abs(newLayerEndPos.y - getY());
-		resize(layerWidth, layerHeight);
-		setLocation(validPoint(getCoords(), newLayerEndPos));
-	}
-
-	public void setStrokeCol(Color col) {
-		this.strokeCol = col;
+		return rectangleGraphics;
 	}
 }
