7f9d5102459bae2b1d8e35929577282587dd713f
Silsby <silsby@vdi-scclab-013.lancs.local>
2023-01-31 18:41:40 +0000

    Blurring on canvas (needs fine tuning)

--- BlurTool.java
+++ BlurTool.java
@@ -1,5 +1,5 @@
 
-
+import java.awt.event.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -10,13 +10,37 @@ public class BlurTool implements Observer{
 
     private LayersHandler layerHandler;
     private Clickable blurBtn;
-
+    private OurCanvas canvas;
+    private int pensize=1;
     
     public BlurTool(OurCanvas canvas)
     {
         this.blurBtn = new Clickable("Blur");
         this.layerHandler = LayersHandler.getLayersHandler(canvas);
+        this.canvas = canvas;
         blurBtn.addObserver(this);
+        canvasListener();
+    }
+
+    private void canvasListener() {
+		canvas.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+                if (blurBtn.isActive())
+                System.out.println("this is currently working");
+			}
+		});
+
+		canvas.addMouseMotionListener(new MouseAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				if (!blurBtn.isActive()) return;
+                try {
+                    blur(e.getX(),e.getY());
+                  }
+                  catch(IOException | InterruptedException error) {
+                    System.out.println("Exception caught");
+                  }
+			}
+		});
     }
 
    
@@ -26,18 +50,22 @@ public class BlurTool implements Observer{
 
 
 	// Blurs the entire layer
-	private void blur() 
+	private void blur(double xCoor, double yCoor) 
         throws IOException, InterruptedException
 	{
-        System.out.println("working");
-        if (blurBtn.isActive())
-        {
-            System.out.println("working");
+       
+        int radius=10;
+        pensize=50;
+        
 		Color color[];
 
 		// converts the current layer into a buffered image
         LayerData currentLayer = layerHandler.getSelectedLayer();
-		BufferedImage input = currentLayer.getImage();
+		BufferedImage bigImage = currentLayer.getImage();
+        BufferedImage convertedImg = new BufferedImage(bigImage.getWidth(), bigImage.getHeight(), bigImage.TYPE_INT_RGB);
+        convertedImg.getGraphics().drawImage(bigImage, 0, 0, null);
+       
+        BufferedImage input = bigImage.getSubimage((int)xCoor, (int)yCoor, pensize/2+50, pensize/2+50);
         
 
 		// Again creating an object of BufferedImage to
@@ -92,29 +120,45 @@ public class BlurTool implements Observer{
 				b1 = b1 / (max);
 				int sum1 = (a1 << 24) + (r1 << 16)
 						+ (g1 << 8) + b1;
-                System.out.println(sum1);
+               
 				output.setRGB(y, x, (int)(sum1));
 			}
 		}
 
 		// Writing the blurred image on the disc where
 		// directory is passed as an argument
-		ImageIO.write(output, "JPEG", new File("/home/silsby/h-drive/scc210/coursework/scc210-2223-grp-67/scc210-2223-grp-67/BlurredImage.jpeg"));
+		
       
+        output = output.getSubimage(radius, radius, pensize/2-10, pensize/2-10);
+        
+        convertedImg.getGraphics().drawImage(output, (int)xCoor, (int)yCoor, null);
+        ImageIO.write(convertedImg, "JPEG", new File("/home/silsby/h-drive/scc210/coursework/scc210-2223-grp-67/scc210-2223-grp-67/BlurredImage.jpeg"));
+        ImageIO.write(output, "JPEG", new File("/home/silsby/h-drive/scc210/coursework/scc210-2223-grp-67/scc210-2223-grp-67/BlurredImage2.jpeg"));
+
+       
         
 
-		// Message to be displayed in the console when
+
+        currentLayer.setImage(convertedImg);
+        layerHandler.updateCanvas();
+
+        
+		
 		// program is successfully executed
 		System.out.println("Image blurred successfully !");
-        }
+        
 	}
+
+    
 
     public Clickable getClickable() {
         return blurBtn;
     }
 
+
     public void update(int thickness) {    
-		
+        System.out.println("SIZE HAS CHANGED");
+		pensize = thickness;
     }
 
 	public void update2(Color col) {
@@ -122,12 +166,13 @@ public class BlurTool implements Observer{
 	}
 
     public void update3() {
-        try {
+       /* try {
             blur();
           }
           catch(IOException | InterruptedException e) {
             System.out.println("Exception caught");
           }
+          */ 
        
     };
 }
