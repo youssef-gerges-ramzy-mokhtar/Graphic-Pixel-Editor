dcdeea92f9b0ec7231e942ff515fc77826d00021
Ditchfield-ogle <ditchfit@vdi-scclab-008.lancs.local>
2023-02-17 15:54:35 +0000

    read text from file

--- Help.java
+++ Help.java
@@ -2,41 +2,57 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.io.*;
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.util.*;
 
 // Help is used Loads Images from the User's Computer into the Program
-class Help{
-    private JMenu helpMenu = new JMenu("Help");
-    private OurCanvas canvas;
+class Help {
+  private JMenu helpMenu = new JMenu("Help");
+  private OurCanvas canvas;
 
-    /** 
-     * creates a button on the menu to save the canvas as a file
-     * @param canvas
-    */
-    public Help(OurCanvas canvas) {
-    	this.canvas = canvas;
-    	addhelpMenuListener();
-    }
+  /**
+   * creates a button on the menu to save the canvas as a file
+   * 
+   * @param canvas
+   */
+  public Help(OurCanvas canvas) {
+    this.canvas = canvas;
+    addhelpMenuListener();
+  }
 
-    /** 
-     * returns the JMenu for this button *
-     * @return
-    */
-    public JMenu getMenu() {
-    	return helpMenu;
-    }
+  /**
+   * returns the JMenu for this button *
+   * 
+   * @return
+   */
+  public JMenu getMenu() {
+    return helpMenu;
+  }
 
-    /** 
-      * Used to attach an event handler to the Menu Button 
-    */ 
-     private void addhelpMenuListener() {
-    	helpMenu.addMenuListener(new MenuListener() {
-            public void menuCanceled(MenuEvent e) {}
+  /**
+   * Used to attach an event handler to the Menu Button
+   */
+  private void addhelpMenuListener() {
+    helpMenu.addMenuListener(new MenuListener() {
+      public void menuCanceled(MenuEvent e) {
+      }
 
-            public void menuDeselected(MenuEvent e) {}
+      public void menuDeselected(MenuEvent e) {
+      }
 
-            public void menuSelected(MenuEvent e) {
-                
-			}
-        });
-    }
+      public void menuSelected(MenuEvent e) {
+        File file = new File("shortcut_list.txt");
+        try {
+          FileReader fr = new FileReader(file);
+          BufferedReader br = new BufferedReader(fr);
+          while (br.ready()) {
+            System.out.println(br.readLine());
+          }
+        } catch (Exception ex) {
+        }
+      }
+    });
+  }
 }
